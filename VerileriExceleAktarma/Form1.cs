90c5c756f3737f5a4fdef939e87b72285a15184f
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-26 19:34:55 +0300

    Initial commit

---
+++ VerileriExceleAktarma/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VerileriExceleAktarma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("www.kodbankasi.gen.tr");
+        }
+
+        private void buttonExceleAktar_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            excel.Visible = true;
+            Microsoft.Office.Interop.Excel.Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+            Microsoft.Office.Interop.Excel.Worksheet sheet1 = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Sheets[1];
+            int StartCol = 1;
+            int StartRow = 1;
+            int j = 0, i = 0;
+
+            //başlıkları yazdırıyoruz...
+            for (j = 0; j < dataGridView1.Columns.Count; j++)
+            {
+                Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[StartRow, StartCol + j];
+                myRange.Value2 = dataGridView1.Columns[j].HeaderText;
+            }
+
+            StartRow++;
+
+            //datagridview içeriğini yazdırıyoruz...
+            for (i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    try
+                    {
+                        Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[StartRow + i, StartCol + j];
+                        myRange.Value2 = dataGridView1[j, i].Value == null ? "" : dataGridView1[j, i].Value;
+                    }
+                    catch
+                    {
+                        ;
+                    }
+                }
+            }
+        }
+    }
+}
